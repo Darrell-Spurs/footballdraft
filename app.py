6ce8df2eb21077899b4314d10f92f4c53c339e38
Darrell-Spurs <darrellyu0413@gmail.com>
2020-09-06 22:40:27 +0800

    0906-01

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
 @app.route('/index')
 def index():
-    return "<h1>Deployed<h1>"
+    return "<h1>Deployed update<h1>"
+
 if __name__ == '__main__':
     app.run(debug=True)
 
