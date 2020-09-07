80e4371aa0ab568248a90c70602989f4f21c1faa
Darrell-Spurs <darrellyu0413@gmail.com>
2020-09-07 22:17:42 +0800

    0907-01

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "<h1>Deployed update<h1>"
-
+@app.route('/about')
+def about():
+    return render_template("/htmls/test.html")
 if __name__ == '__main__':
     app.run(debug=True)
 
