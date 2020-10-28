44d1548f0be9d89d5d986dfd420c62c92f2a0cce
Darrell-Spurs <darrellyu0413@gmail.com>
2020-10-28 15:07:33 +0800

    1028_lots_of_updates

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ import json
 import pymysql
 import pymysql.cursors
 import csv
+import time
 app = Flask(__name__)
 
-#io = SocketIO(app=app, log=True, async_mode="eventlet")
-io = SocketIO(app=app, async_mode="eventlet")
+async_mode="threading"
+io = SocketIO(app=app, async_mode=async_mode)
+#io = SocketIO(app=app, async_mode=async_mode)
 
 def db_connect():
     global connection
@@ -50,6 +52,23 @@ def index():
     return render_template("players.html",
                            all_players=result)
 
+
+@app.route('/round/<r>')
+def get_round(r):
+    db_connect()
+    with connection.cursor() as cursor:
+        sql = f"SELECT ID,Player,Position,Club,Nation,Age FROM all_players WHERE ROUND={r}"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        connection.commit()
+    connection.close()
+    print(type(result[0]))
+    for elem in result:
+        while (elem['Club'].find(" ") != -1):
+            f=elem['Club'].find(" ")
+            elem['Club'] = elem['Club'][:f] + "_" + elem['Club'][f + 1:]
+    return render_template('rounds.html',r=int(r),result=result)
+
 @app.route('/index')
 def connect_test():
     with connection.cursor() as cursor:
@@ -74,52 +93,61 @@ def teams():
 
     return render_template("teams.html", countries = nation_dict)
 
+@app.route('/transactions/real')
+def transactions_real():
+    return render_template('transaction.html')
+
+@app.route('/transactions/records')
+def transactions_records():
+    return render_template('transaction.html')
+
+@app.route('/transactions/tool')
+def transactions_tool():
+    return render_template('transaction.html')
+
 @app.route('/teams/<nteam>')
 def national(nteam):
-    action_add = request.args.get('a')
-    action_del = request.args.get('d')
-    if(action_add!=None):
-        add_to_N(nteam,action_add)
-        return redirect('/teams/%s'%nteam)
-    elif (action_del != None):
-        del_from_N(action_del)
-        return redirect('/teams/%s' % nteam)
-    else:
-        db_connect()
-        national=[]
-        nation_dict=dict()
-        with open('Official.csv', newline="") as file:
-            rows=csv.reader(file)
-            for elem in rows:
-                national.append(elem)
-            countries = national[1:13]
+    db_connect()
+    national=[]
+    nation_dict=dict()
 
-            for team in countries:
-                while "" in team:
-                    team.remove("")
-                nation_dict[team[0]] = team[1:]
-            sql_str=""
-            for member in nation_dict[nteam]:
-                sql_str+=f'Nation = "{member}" OR '
-            sql_str=sql_str[:-4]
+    #read countries
+    with open('Official.csv', newline="") as file:
+        rows=csv.reader(file)
+        for elem in rows:
+            national.append(elem)
+        countries = national[1:13]
 
-            position=[['GK'],['LB','CB','RB'],['CAM','CM','CDM','LM','RM'],['LW','RW','ST','CF']]
-            groups = ["Goalkeepers","Defenders","Midfielders","Attackers"]
-            results = []
-            for poses in position:
-                sql_pos = ""
-                for pos in poses:
-                    sql_pos += f'Position = "{pos}" OR '
-                sql_pos = sql_pos[:-4]
+        #get country list from csv
+        for team in countries:
+            while "" in team:
+                team.remove("")
+            nation_dict[team[0]] = team[1:]
+        sql_str=""
+        #create get sql for searching available players
+        for member in nation_dict[nteam]:
+            sql_str+=f'Nation = "{member}" OR '
+        sql_str=sql_str[:-4]
 
-                with connection.cursor() as cursor:
-                    sql=f"SELECT * FROM all_players WHERE Owner = 'Darrell' AND ({sql_str}) AND ({sql_pos})"
-                    cursor.execute(sql)
-                    result=cursor.fetchall()
-                    results.append(result)
-                connection.commit()
+        #classify positions
+        position=[['GK'],['LB','CB','RB'],['CAM','CM','CDM','LM','RM'],['LW','RW','ST','CF']]
+        groups = ["Goalkeepers","Defenders","Midfielders","Attackers"]
+        results = []
+        for poses in position:
+            sql_pos = ""
+            for pos in poses:
+                sql_pos += f'Position = "{pos}" OR '
+            sql_pos = sql_pos[:-4] #remove the last 'OR'
+
+            #select players from each category
+            with connection.cursor() as cursor:
+                sql=f"SELECT * FROM all_players WHERE Owner = 'Darrell' AND ({sql_str}) AND ({sql_pos})"
+                cursor.execute(sql)
+                result=cursor.fetchall()
+                results.append(result)
+            connection.commit()
         connection.close()
-        return render_template("national.html", results=results, groups=groups, nteam=nteam)
+    return render_template("national.html", results=results, groups=groups, nteam=nteam)
 
 @app.route('/user/<username>')
 def user(username):
@@ -168,5 +196,99 @@ def home_search(msg):
     emit('builder_result',{'data':result})
     print(result)
 
+@io.on("save_squad")
+def add_add_squad(msg):
+    recieved = msg['players']
+    db_connect()
+    with connection.cursor() as cursor:
+        try:
+            recieved = tuple(recieved)
+            sql = f'''INSERT INTO squads (NAME,P1,P2,P3,P4,P5,P6,P7,P8,P9,P10,P11,P12,P13,P14,P15,P16,P17,P18)
+                  VALUES {recieved}'''
+            cursor.execute(sql)
+            connection.commit()
+        except:
+            for i in range(len(recieved)):
+                if recieved[i]=='d Outline':
+                    recieved[i]=0
+                recieved = tuple(recieved)
+            sql = f'''INSERT INTO squads (NAME,P1,P2,P3,P4,P5,P6,P7,P8,P9,P10,P11,P12,P13,P14,P15,P16,P17,P18)
+                    VALUES {recieved}'''
+            cursor.execute(sql)
+            connection.commit()
+    connection.close()
+
+@io.on('natTeam_Add')
+def natTeam_Add(msg):
+    add_to_N(msg['nation'],msg['player'])
+    print(msg['player'],msg['nation'])
+
+@io.on('natTeam_Del')
+def natTeam_Del(msg):
+    del_from_N(msg['player'])
+    print(msg['player'])
+    emit("nat_added")
+
+@io.on('testtest')
+def testtest(msg):
+    print(msg)
+    emit("nat_deleted")
+
+@io.on('trade')
+def trade(msg):
+    print('Recieved:',msg)
+    req = msg['req']
+    out=msg['out']
+    tin=msg['tin']
+    db_connect()
+    with connection.cursor() as cursor:
+        rec=""
+        if req=="Ben":
+            rec="Darrell"
+        else:
+            rec="Ben"
+        Update1 = "UPDATE all_players SET Owner='%s' WHERE Player='%s'"%(rec,out)
+        cursor.execute(Update1)
+
+        Update2 = "UPDATE all_players SET Owner='%s' WHERE Player='%s'"%(req,tin)
+        cursor.execute(Update2)
+
+        connection.commit()
+    connection.close()
+
+@io.on('pick')
+def pick(msg):
+    print("Recieved:",msg)
+    drop=msg['drop']
+    add=msg['add']
+    pos=msg['pos']
+    nat=msg['nat']
+    db_connect()
+    with connection.cursor() as cursor:
+        drop_sql = f"SELECT ID FROM all_players WHERE Player='{drop}'"
+        cursor.execute(drop_sql)
+        ID = cursor.fetchone()['ID']
+
+        add_sql = f"UPDATE all_players SET Player='{add}',Position='{pos}',Nation='{nat}' WHERE ID={ID}"
+        print(add_sql)
+        cursor.execute(add_sql)
+        connection.commit()
+    connection.close()
+
+@io.on('suggest')
+def suggest(msg):
+    try:
+        sug = msg['sug']
+        db_connect()
+        with connection.cursor() as cursor:
+            sql = f"SELECT player,club FROM all_players WHERE player LIKE '%{sug}%';"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            print(result)
+        connection.close()
+        print("emitted")
+        emit('sug_result',{'result': result})
+    except:
+        pass
 if __name__ == '__main__':
     io.run(app)
